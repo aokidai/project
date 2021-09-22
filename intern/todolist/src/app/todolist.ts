36b0c0b30c29161f4e8d51285c10d40fe7df5b5e
aokidaisuke <aokidaisuke91@gmail.com>
2021-09-22 19:51:43 +0900

    upload

---
+++ intern/todolist/src/app/todolist.ts
@@ -0,0 +1,11 @@
+import { Todo } from "./todo";
+
+export const TODO: Todo[] = [
+    { id: 1, name: 'Angurla' },
+    { id: 2, name: 'HTML5 & CSS' },
+    { id: 3, name: 'PHP' },
+    { id: 4, name: '.NET' },
+    { id: 5, name: 'Git' },
+    { id: 6, name: 'Akita' },
+    { id: 7, name: 'RxJS' },
+]
